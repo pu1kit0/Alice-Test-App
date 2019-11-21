b16f1478d5d3df009b14fda745c0aa6844c5aea8
ParasJain-dev <parasjain@moengage.com>
2019-11-21 15:21:47 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,12 +1,8 @@
 wdw
 export s3_key='PARAY5F8IA5LEOBNQ8JA'
-export s3_secret='pe5JAnlIN7veVs2A0EaC0wIOz6CQVrx8EQvslPRi'
 export git_key=''
 export git_secret=''
 export slack_token='xoxb-99081796999-pr0JDEnScESprezGWsFM8678'
 export slack_token='xoxb-99081796999-pr0JDEnScESprezGWsFM8678'
-export s3_secret='pe5JAnlIN7veVs2A0EaC0wIOz6CQVrx8EQvslPRi'
-export s3_secret='pe5JAnlIN7veVs2A0EaC0wIOz6CQVrx8EQvslPRi'
-export s3_secret='pe5JAnlIN7veVs2A0EaC0wIOz6CQVrx8EQvslPRi'
 
 export slack_token='xoxb-99081796999-pr0JDEnScESprezGWsFM8678'
